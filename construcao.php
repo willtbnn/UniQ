e7cab32231fbcf990a891aeb58c6dfa4752d836f
Jorge Luiz <jlbnunes@live.com>
2019-11-18 20:37:26 -0300

    construindo a pagina construçao

--- construcao.php
+++ construcao.php
@@ -1,8 +1,135 @@
-<section class="container-fluid pg-light pt-5 mb-0">
-    <div class="container text-center">
+<header class="container-fluid  pt-5 mb-0 servicos-bg">
+    <div class="container text-center ">
         <img class="img-fluid py-5 icon" src="assets/images/slogan/construcao.png"/>
-        <h1 class="h2">Pagina em construção </h1>
-        <p class="lead">Mais sinta-se a vontade para pedir seu orçamento, temos uma equipe especializada, pronta para atender.</p>
-    <h2 class="display-2"><a href="pedidos.php" class="btn btn-outline-warning btn-lg mt-4 bgf"><span class="carousel-control-next-icon"></span>Peça Orçamento</a></h2>
     </div>    
-</section>
+</header><!--
+<article class="container-fluid my-5 py-5 text-center">
+        <p class=" text-monospace">
+            Com sede em vários estados, somos especializados em obras comerciais, industriais, casas de alto padrão e com setor especializado também em obras e reformas de pequeno porte.
+        </p>
+</article>-->
+<div class="container-fluir borda-cons"></div>
+<article class="container-fluid border-top border-bottom border-info pb-5 bg-info">
+    <h1 class="h3 text-center py-5 text-light">Todos os serviços de que você precisa em um só lugar.</h1>
+    <div class="row p-0 bg-info">
+        <div class="col-md-2 col-sm-12 construcao-1 py-5">
+            <p class="sombra-branca text-center ">Construção civil</p>
+        </div>
+        <div class="col-md-2 col-sm-12 construcao-2 py-5">
+            <p class="sombra-branca-2 text-center ">Hidraulica</p>
+        </div>
+        <div class="col-md-2 col-sm-12 construcao-3 py-5">
+            <p class="sombra-branca-3 text-center ">Drywall&Gesso</p>
+        </div>
+        <div class="col-md-2 col-sm-12 construcao-4 py-5">
+            <p class="sombra-branca-4 text-center ">Predial</p>
+        </div>
+        <div class="col-md-2 col-sm-12 construcao-6 py-5">
+            <p class="sombra-branca-6 text-center ">Reformas em gerais</p>
+        </div>
+        <div class="col-md-2 col-sm-12 construcao-5 py-5">
+            <p class="sombra-branca-5 text-center ">Reforma predial</p>
+        </div>
+    </div>
+</article>
+<div class="container-fluir borda-cons"></div>
+<article class="container-fluid bg-construcao">
+    <div class="container row  py-5">
+        <div class="col-4"></div>
+        <div class="col-md-7 col-sm-12  align-self-end">
+        <h2>SOLUÇÃO PARA TODAS TIPOS DE OBRAS.</h2>
+        <p class="lead">Com um equipe qualificada e dedicada, com alto nível de precisão e prazo apropriados as necessidades para a sua obra ou serviços.</p>
+        </div>
+    </div>
+</article>
+<article class="container">
+    <div class="text-center texto-constr my-5">
+        <h3 classs="h1 ">Nossas Soluções</h3>
+    </div>
+    <div class="container-fluir borda-cons mb-5"></div>
+    <ul class="">
+        <li class="">Instalações</li>
+        <li class="">Montagem</li>
+        <li class="">Vendas e Manutenção de Aquecedores Solar</li>
+        <li class="">Instalações Hidráulicas</li>
+    </ul>
+    <ul class="mt-2">
+        <li class="texto-constr font-weight-bold">Prestação de Serviços de Mão de Obra em:</li>
+        <p class="font-weight-lighter"> Construções, reformas, pinturas.</p>
+    </ul>
+    <ul class="">
+        <li class="">Limpeza</li>
+        <li class="">Portaria</li>
+        <li class="">Comércio de materias de construção</li>
+    </ul>
+    <ul class="mt-2">
+        <li class="texto-constr font-weight-bold">Artefatos Gesso e Sistemas Construtivos a Seco</li>
+        <p> Gesso, drywall, stell frame, forros, divisórias, forros removíveis, pisos</p>
+    </ul>
+    <div class="text-center my-5">
+        <p class="text-monospace">Serviços de execução por adiministração, empreitada ou sub-empreitada, de obras de construção civil, hidráulica ou elétrica e de outras obras semelhantes</p>
+    </div>
+</article>
+<article class="container-fluid bg-info text-center">
+    <button type="button" class="btn btn-lg btn-outline-info">
+        <a href="https://grupouniq.com.br/pedidos" class="text-decoration-none">Entre em contato</a>
+    </button>
+</article>
+<div class="container-fluir borda-cons"></div>
+<article class="container text-center py-5">
+    <h3 class="h2 texto-constr">CLIENTES E FORNECEDORES</h3>
+    <div class="container-fluir borda-cons mb-5"></div>
+    <h4 class="display-6">CONDOMÍNIOS:</h4>
+    <p class="h3 texto-constr">The Taj Office Tower - Vila Olímpica</p>
+    <p class="h3 texto-constr">Edifício Safira - Cerqueira Cesar</p>
+    <div class="row py-5">
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/APM.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/bagaggio.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/construtoraJL.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/fiocruz.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+           <img src="assets/images/construcao/cliente-fonercedores/mci.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/mundo-verde.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/não-sei.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/officilaba.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/racional.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/random.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/rhenus.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/sony.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/studioG.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/umuarama.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/wgt.png" class="img-fluid">
+        </div>
+        <div class="col-md-2 col-sm-4 pt-2">
+            <img src="assets/images/construcao/cliente-fonercedores/wtorres.png" class="img-fluid">
+        </div>
+    </div>
+</article>
